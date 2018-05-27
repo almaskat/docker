def854d9d239d14f7b2552e86ea408f04163cc75
Iqbal Maskatiya <iqbal.maskatiya@gmail.com>
2018-05-27 00:05:26 -0400

    made guest root in docker

--- SHARE-DC/samba.sh
+++ SHARE-DC/samba.sh
@@ -1,7 +1,6 @@
 sudo docker run -it -p 139:139 -p 445:445 -v /media/1TB:/mount:rw -d dperson/samba \
     -n \
-    -g "security = share"
-    -g "guest account = nobody" \
+    -g "guest account = root" \
     -w "MASKATIYA.XYZ" \
     -s "MEDIA;/mount/MEDIA;yes;yes;yes;all" \
     -s "TEMP;/mount/TEMP;yes;no;yes;all"
