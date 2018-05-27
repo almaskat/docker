73e9bab0399607656c67afa58432e531fd5bcbed
Iqbal Maskatiya <iqbal.maskatiya@gmail.com>
2018-05-27 00:03:15 -0400

    added possible fix for no password

--- SHARE-DC/samba.sh
+++ SHARE-DC/samba.sh
@@ -1,5 +1,7 @@
 sudo docker run -it -p 139:139 -p 445:445 -v /media/1TB:/mount:rw -d dperson/samba \
     -n \
+    -g "security = share"
+    -g "guest account = nobody" \
     -w "MASKATIYA.XYZ" \
     -s "MEDIA;/mount/MEDIA;yes;yes;yes;all" \
     -s "TEMP;/mount/TEMP;yes;no;yes;all"
